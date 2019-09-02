dd93f1f25e7e7901498871c8661e62e2eca3d2a0
BELLINI <tais.bellini@hp.com>
2019-09-02 12:45:04 -0300

    Sec 3.6 - exemplo numerico

--- exercicios_livro.R
+++ exercicios_livro.R
@@ -1,4 +1,36 @@
 # ECONOMETRIA BASICA #
+
+### CAP 3 ###
+## 3.6 Exemplo numerico 
+## Consumo familiar semanal (y), renda familiar semanal (x)
+
+X = c(80, 100, 120, 140, 160, 180, 200, 220, 240, 260)
+y = c(70, 65, 90, 95, 110, 115, 120, 140, 155, 150)
+
+X.matr = cbind(1, X)
+
+result = f.mqo(X.matr, y)
+b0 = result$coef[1]
+b1 = result$coef[2]
+
+#vemos que o reusultado bate com o do livro
+
+# graficos
+plot(X, y)
+regress = function (x){b0 + b1*x}
+curve(regress, min(X), max(X), add=T)
+abline(h=mean(y), color="red")
+abline(v=mean(X))
+
+#o quanto a linha regressiva encaixa nos dados?
+## r2 (minusculo pois eh amostra)
+## r2 = 1 - SQR/SQT
+
+sqr = sum((result$res)^2)
+sqt = sum((y-mean(y))^2)
+r2 = 1 - (sqr/sqt)
+# resultado: 96%
+
 # Cap 3 - Exercicio 2 #
 
 X1 = matrix(c(1, 4, 5, 6),nrow = 4, ncol = (2-1))
